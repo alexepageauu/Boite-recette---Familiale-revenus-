fa46d688996d63590fc40ce924a3db2227bd0df2
alexepageauu <alexe2001@live.ca>
2026-09-11 17:10:04 -0400

    Add files via upload

--- link-preview.js
+++ link-preview.js
@@ -58,7 +58,7 @@ function extractRecipeJsonLd(html) {
   return null;
 }
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   if (req.method !== "POST") {
     return res.status(405).json({ error: "Méthode non autorisée" });
   }
